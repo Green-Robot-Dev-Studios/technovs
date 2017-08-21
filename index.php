b12cb72a6a748bbaef66484fae1a2982e3fe10e9
BuildTools <unconfigured@null.spigotmc.org>
2017-08-21 09:33:55 -0400

    New Website

---
+++ index.php
@@ -0,0 +1,48 @@
+<html>
+	<head>
+		<title>TechnoVS</title>
+		<link rel="apple-touch-icon" sizes="180x180" href="lib/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="lib/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="lib/favicon-16x16.png">
+		<link rel="manifest" href="lib/manifest.json">
+		<link rel="mask-icon" href="lib/safari-pinned-tab.svg" color="#5bbad5">
+		<link rel="stylesheet" href="css/style.css">
+		<meta name="theme-color" content="#ffffff">
+	</head>
+	<body>
+		<div id="particle"></div>
+			<div id="overlay">
+				<div id="header">
+					<img src="lib/logo.png">
+				</div>
+				<div id="content">
+					<div id="nav">
+						<h2>Links</h2>
+						<ul>
+							<h3><li><a class="selected" href="">Home</a></li>
+							<li><a href="about.php">About Our Site</a></li>
+							<li><a href="top.php">Top rated articles</a></li>
+							<li><a href="goal.php">Our Goal</a></li>
+							<li><a href="sitemap.php">Sitemap</a></li></h3>
+						</ul>
+					</div>
+					<div id="home">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed bibendum lacus, eu ultricies neque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus at imperdiet enim. Nulla vitae placerat odio. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent eu laoreet sapien, ut tristique lacus. Donec ut massa dignissim, aliquam nisl quis, mattis metus. Sed id malesuada ipsum, nec maximus nunc. Vestibulum accumsan lobortis nisl ac condimentum.</p>
+
+						<p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas egestas augue eget lectus suscipit convallis. Vivamus convallis dolor justo, eget vehicula ex semper a. Etiam quis tincidunt mi. Quisque quis dui purus. Aenean in libero vel sem ultricies rhoncus. Proin ut nibh nunc. Nullam id augue varius, suscipit enim eu, facilisis magna. Mauris pretium eros ex, sit amet sagittis lacus ullamcorper sed. Nam a lobortis augue. Ut condimentum neque vitae tellus viverra, vitae vehicula urna scelerisque. Praesent efficitur nibh quis est ornare scelerisque sit amet at dolor.</p>
+
+						<p>Pellentesque aliquet in est at malesuada. Praesent vitae sem eu ante porttitor tempus eget nec nisl. Suspendisse scelerisque auctor egestas. In vel varius magna. Curabitur maximus, dui eu euismod tincidunt, est massa viverra quam, sed commodo purus arcu non sapien. In ipsum orci, rutrum non maximus vel, vestibulum vel dolor. Nunc eu luctus sapien, eget elementum diam. Fusce consectetur laoreet auctor. In metus libero, suscipit eget quam a, tincidunt ultrices massa.</p>
+
+						<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin ut consequat erat, ac vehicula sapien. Sed laoreet lacinia accumsan. Proin sagittis molestie elit eu suscipit. Aliquam erat volutpat. Aenean molestie elit vitae orci vehicula iaculis. Donec eu mattis lectus. Fusce congue a lectus quis tincidunt. Fusce cursus lectus dolor, eget hendrerit ex lobortis et. Donec id arcu sed leo dignissim ullamcorper eget nec ante.</p>
+
+						<p>Donec mattis, urna vitae commodo euismod, augue mi vulputate felis, eu tincidunt est urna vitae odio. Sed cursus pharetra diam sit amet euismod. Vivamus sit amet augue pharetra, convallis mi non, finibus massa. Vestibulum cursus tincidunt placerat. Donec egestas turpis id augue varius, a mattis orci ultrices. Donec posuere tempus erat, in dapibus sapien consectetur et. Integer mattis libero non elit pretium, ac tincidunt sem volutpat. In posuere pellentesque posuere. Nullam sem ligula, iaculis vitae lacinia ac, faucibus in velit. Pellentesque molestie sit amet augue ac pellentesque. Integer molestie metus leo. Phasellus tellus sem, efficitur at dapibus vitae, commodo id nibh. Fusce commodo a leo vitae pharetra. Morbi tincidunt dapibus molestie. Nulla sagittis dictum ligula, vel tristique nisl suscipit a. Aenean euismod in augue eu mollis.</p>
+					</div>
+					<div id="footer">
+					Copyright &copy; 2017 Nicholas Ficara
+					</div>
+				</div>	
+			</div>
+		<script src="js/particles.min.js"></script>
+		<script src="js/index.js"></script>
+	</body>
+</html>
